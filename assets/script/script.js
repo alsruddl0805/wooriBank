2d0486c106a2b2befc629aff4a7fc5ff291d43dd
alsruddl0805 <alsruddl0805@naver.com>
2020-09-28 20:25:57 +0900

    wooribank

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,13 +1,13 @@
-// 로그인 & 공인인증 하위메뉴 구현
-$("#mainBtn>li").mouseover(function(){
-    $(this).children(".subBtn").stop().show();
+// // 로그인 & 공인인증 하위메뉴 구현
+$("#mainBtn li").mouseover(function(){
+    $(this).children(".subBtn").show();
 });
 
-$("#mainBtn>li").mouseout(function(){
-    $(this).children(".subBtn").stop().hide();
+$("#mainBtn li").mouseout(function(){
+    $(this).children(".subBtn").hide();
 });
 
-// GNB 메뉴 구현
+// header GNB 메뉴 구현
 $("#mainMenu>li").mouseover(function(){
     $(this).children(".subMenu").stop().show();
 });
@@ -15,3 +15,36 @@ $("#mainMenu>li").mouseover(function(){
 $("#mainMenu>li").mouseout(function(){
     $(this).children(".subMenu").stop().hide();
 });
+
+// footer LNB 메뉴 구현
+$("#footerMainMenu>li").mouseover(function(){
+    $(this).children(".footerSubMenu").stop().show();
+});
+
+$("#footerMainMenu>li").mouseout(function(){
+    $(this).children(".footerSubMenu").stop().hide();
+});
+
+// footer infoImg hover
+$("#infoImg>a").mouseover(function(){
+    $(this).children("span").stop().show();
+});
+
+$("#infoImg>a").mouseout(function(){
+    $(this).children("span").stop().hide();
+});
+
+// slideShow
+
+setInterval(fnSlide, 4000);
+
+function fnSlide() {
+   $("#slideFrame").animate(
+                               {"margin-left": "-980px"},
+                               2000,
+                               function(){
+                                   $("#slideFrame").css({"margin-left": "0"});
+                                   $("#slideFrame>a:first").insertAfter("#slideFrame>a:last");
+                               }
+                           );
+}
