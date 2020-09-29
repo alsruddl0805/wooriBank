7e67f58dd03facf5ac7ea57cc025d0b927e17352
alsruddl0805 <alsruddl0805@naver.com>
2020-09-29 17:15:17 +0900

    wooribank

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,4 @@
-// // 로그인 & 공인인증 하위메뉴 구현
+// buttonArea
 $("#mainBtn li").mouseover(function(){
     $(this).children(".subBtn").show();
 });
@@ -7,7 +7,7 @@ $("#mainBtn li").mouseout(function(){
     $(this).children(".subBtn").hide();
 });
 
-// header GNB 메뉴 구현
+// header GNB
 $("#mainMenu>li").mouseover(function(){
     $(this).children(".subMenu").stop().show();
 });
@@ -16,7 +16,7 @@ $("#mainMenu>li").mouseout(function(){
     $(this).children(".subMenu").stop().hide();
 });
 
-// footer LNB 메뉴 구현
+// footer LNB
 $("#footerMainMenu>li").mouseover(function(){
     $(this).children(".footerSubMenu").stop().show();
 });
@@ -35,16 +35,101 @@ $("#infoImg>a").mouseout(function(){
 });
 
 // slideShow
+var slideSet = setInterval(Slide, 3000);
 
-setInterval(fnSlide, 4000);
-
-function fnSlide() {
-   $("#slideFrame").animate(
-                               {"margin-left": "-980px"},
-                               2000,
-                               function(){
-                                   $("#slideFrame").css({"margin-left": "0"});
-                                   $("#slideFrame>a:first").insertAfter("#slideFrame>a:last");
-                               }
-                           );
+function Slide() {
+   $("#slideFrame a:first").fadeOut(2000,function(){
+       $(this).insertAfter("#slideFrame a:last");
+   });
+   $("#slideFrame a:nth-child(2)").fadeIn(3000);
 }
+
+// colorSlideShow
+var colorSet = setInterval(colorSlide, 3000);
+
+function colorSlide() {
+   $("#slideBack p:first").fadeOut(2000,function(){
+       $(this).insertAfter("#slideBack p:last");
+   });
+   $("#slideBack p:nth-child(2)").fadeIn(3000);
+}
+
+// pause&play Slide
+var pauseBtn = document.querySelector(".fa-pause");
+pauseBtn.onclick = stopSlide;
+
+var playBtn = document.querySelector(".fa-play");
+playBtn.onclick = goSlide;
+playBtn.style.display = "none";
+playBtn.style.position = "relative";
+playBtn.style.top = "10px";
+
+function stopSlide() {
+    playBtn.style.display = "block";
+    pauseBtn.style.display = "none";
+    clearInterval(slideSet);
+    clearInterval(colorSet);
+}
+
+function goSlide() {
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "block";
+    pauseBtn.style.position = "relative";
+    pauseBtn.style.top = "10px";
+    setInterval(Slide,3000);
+    setInterval(colorSlide,3000);
+}
+
+// slideDot
+$("#slideDot li").click(function(){
+    clearInterval(slideSet);
+    clearInterval(colorSet);
+});
+
+$("#slideDot li:first-child").click(function(){
+    $("#slideFrame a#one").fadeIn();
+    $("#slideFrame a#two").fadeOut();
+    $("#slideFrame a#three").fadeOut();
+    $("#slideFrame a#four").fadeOut();
+
+    $("p#slide_01").fadeIn();
+    $("p#slide_02").fadeOut();
+    $("p#slide_03").fadeOut();
+    $("p#slide_04").fadeOut();
+});
+
+$("#slideDot li:nth-child(2)").click(function(){
+    $("#slideFrame a#one").fadeOut();
+    $("#slideFrame a#two").fadeIn();
+    $("#slideFrame a#three").fadeOut();
+    $("#slideFrame a#four").fadeOut();
+
+    $("p#slide_01").fadeOut();
+    $("p#slide_02").fadeIn();
+    $("p#slide_03").fadeOut();
+    $("p#slide_04").fadeOut();
+});
+
+$("#slideDot li:nth-child(3)").click(function(){
+    $("#slideFrame a#one").fadeOut();
+    $("#slideFrame a#two").fadeOut();
+    $("#slideFrame a#three").fadeIn();
+    $("#slideFrame a#four").fadeOut();
+
+    $("p#slide_01").fadeOut();
+    $("p#slide_02").fadeOut();
+    $("p#slide_03").fadeIn();
+    $("p#slide_04").fadeOut();
+});
+
+$("#slideDot li:nth-child(4)").click(function(){
+    $("#slideFrame a#one").fadeOut();
+    $("#slideFrame a#two").fadeOut();
+    $("#slideFrame a#three").fadeOut();
+    $("#slideFrame a#four").fadeIn();
+
+    $("p#slide_01").fadeOut();
+    $("p#slide_02").fadeOut();
+    $("p#slide_03").fadeOut();
+    $("p#slide_04").fadeIn();
+});
